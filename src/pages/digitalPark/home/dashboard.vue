2871a436f500681b068faf48ad37351bf3829ba4
guanys <616393451@qq.com>
2019-11-07 19:39:40 +0800

    update dashboard

--- src/pages/digitalPark/home/dashboard.vue
+++ src/pages/digitalPark/home/dashboard.vue
@@ -41,10 +41,10 @@
         <img v-if="pageFlag==2" src="../../../../static/image/digitalPark/unity_priview.png"
              class="unity_priview"
              alt="">
-        <!--<iframe v-if="pageFlag==1"-->
-                <!--src="../../../../static/HomePage/index.html"-->
-                <!--frameborder="0"-->
-                <!--class="unity-frame"></iframe>-->
+        <iframe v-if="pageFlag==1"
+                src="../../../../static/HomePage/index.html"
+                frameborder="0"
+                class="unity-frame"></iframe>
       </div>
       <div class="dashboard-right">
         <draggable :list="proModuleList2"
@@ -72,7 +72,6 @@
                  >{{item.name}}</div>
                </el-tooltip>
              </div>
-
         </div>
         </draggable>
       </div>
