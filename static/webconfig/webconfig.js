4bbeb4ed1396476b888eea4c562dd4038bd14872
lxh <283700113@qq.com>
2019-11-18 14:25:18 +0800

    更新vibeweb

--- static/webconfig/webconfig.js
+++ static/webconfig/webconfig.js
@@ -1,4 +1,4 @@
 window.gateway = "http://localhost:8080"
 window.websocketBaseUrl = 'ws://192.168.1.224:8088'
-window.OLDPROJECTHOME = "http://192.168.1.69:9002/html/index.html"
+window.OLDPROJECTHOME = "http://localhost:8090/vibe-web"
 
