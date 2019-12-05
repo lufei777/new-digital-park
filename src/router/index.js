ee061a6e844ab0d3f63d50c695f10e5c7799269b
lxh <283700113@qq.com>
2019-12-05 16:26:20 +0800

    租赁管理

--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,12 @@ import vibeWeb from './vibeWeb'
 // 项目私有路由
 import EnergyRouter from './energy-router'
 import DigitalParkRouter from './digital-park-router'
+import LeaseManage from './lease-manage'
 
 import helloRouter from './hello-router'
 
 // 数字园区
-let DigitalRouters = flattenDeep([DigitalParkRouter, AssetManage, EnergyRouter, vibeWeb]);
+let DigitalRouters = flattenDeep([DigitalParkRouter, AssetManage, EnergyRouter, vibeWeb, LeaseManage]);
 
 // const routes = flattenDeep([DigitalRouters]);
 Vue.use(Router)
@@ -23,15 +24,15 @@ const router = new Router({
 })
 //
 
-router.beforeEach((to, from, next) => {
-  if(typeof to.meta.loginCheck === 'boolean' && !to.meta.loginCheck){
+/* router.beforeEach((to, from, next) => {
+  if (typeof to.meta.loginCheck === 'boolean' && !to.meta.loginCheck) {
     next()
-  }else{
+  } else {
     axois.get('/oaApi/user/login').then(() => {
       next()
     }).catch(() => {
 
     })
   }
-})
+}) */
 export default router
