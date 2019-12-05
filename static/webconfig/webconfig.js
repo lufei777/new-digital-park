995799bcbebabb21a338f451f92f1768dc6eb89c
guanys <616393451@qq.com>
2019-12-05 10:02:14 +0800

    update

--- static/webconfig/webconfig.js
+++ static/webconfig/webconfig.js
@@ -1,8 +1,8 @@
 window.gateway = "http://localhost:8080"
 window.websocketBaseUrl = 'ws://192.168.1.224:8088'
-window.vibeWebUrl = "http://localhost:8090/vibe-web"
+window.vibeWebUrl = "http://192.168.1.96:8081/vibe-web"
 window.OLDPROJECTHOME = "http://localhost:8090/vibe-web/html"
 
-window.vibeWebGateWay = 'http://192.168.1.87:8080'
-window.oaApiGateWay = 'http://39.98.130.147:9003'
+// window.vibeWebGateWay = 'http://192.168.1.87:8080'
+// window.oaApiGateWay = 'http://39.98.130.147:9003'
 
