52641c2174c4b04deef20d4edf914d537f459593
lxh <283700113@qq.com>
2019-12-10 11:21:15 +0800

    字典

---
+++ src/utils/dic/leaseManage.js
@@ -0,0 +1,34 @@
+export const LeaseManageDic = {
+    // 是否可出租
+    isRent: [
+        {
+            label: '是',
+            value: 1
+        }, {
+            label: '否',
+            value: 2
+        }
+    ],
+    // 房屋出租状态
+    HouseStatus: [
+        {
+            label: '已租',
+            value: 1
+        },
+        {
+            label: '待租',
+            value: 2
+        }
+    ],
+    // 总价单位
+    PriceType: [
+        {
+            label: '元/月',
+            value: 1
+        },
+        {
+            label: '元/天',
+            value: 2
+        }
+    ]
+}
