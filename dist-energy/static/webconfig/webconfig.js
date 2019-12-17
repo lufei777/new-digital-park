76113c34572f87f2eebe28a6b34ebeed1f7a3bec
guanys <616393451@qq.com>
2019-12-17 17:44:23 +0800

    update client and btn-io

--- dist-energy/static/webconfig/webconfig.js
+++
@@ -1,8 +0,0 @@
-window.gateway = "http://localhost:8080"
-window.websocketBaseUrl = 'ws://192.168.1.224:8088'
-window.vibeWebUrl = "http://192.168.1.96:8081/vibe"
-window.OLDPROJECTHOME = "http://l92.168.1.96/vibe"
-
-// window.vibeWebGateWay = 'http://192.168.1.87:8080'
-// window.oaApiGateWay = 'http://39.98.130.147:9003'
-
