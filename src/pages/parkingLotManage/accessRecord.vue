f1cecb4d38d5bb65bc8ec3b2a0ddc53a2b75386a
lxh <283700113@qq.com>
2019-12-19 15:54:03 +0800

    停车场管理

---
+++ src/pages/parkingLotManage/accessRecord.vue
@@ -0,0 +1,267 @@
+<template>
+  <div>
+    <div id="houseproperty-form" class="panel">
+      <miForm
+        :ref="leaseManageForm.ref"
+        :options="leaseManageForm"
+        v-model="model"
+        @submit="submit"
+        @reset-change="resetChange"
+      >
+        <template slot="btn" slot-scope="obj">
+          <div>
+            <el-button :disabled="obj.disabled" type="primary" @click="search(obj)">搜索</el-button>
+            <el-button :disabled="obj.disabled" @click="clearForm(obj)">清除</el-button>
+          </div>
+        </template>
+      </miForm>
+    </div>
+
+    <div class="table panel">
+      <miTable :ref="leaseManageTable.ref" :tableConfig="leaseManageTable">
+        <template slot="custom-top" slot-scope="obj">
+          <el-button :size="obj.size" type="primary" @click="addedProperty(obj)">新增</el-button>
+          <el-button :size="obj.size" type="primary" @click="bulkImport(obj)">批量导入</el-button>
+          <el-button :size="obj.size" type="primary" @click="bulkDel(obj)">批量删除</el-button>
+          <el-button :size="obj.size" type="primary" @click="bulkEdit(obj)">批量编辑</el-button>
+        </template>
+        <template slot="operation" slot-scope="obj">
+          <el-button type="text" @click="propertyDetail(obj)">详情</el-button>
+          <el-button type="text" @click="propertyEdit(obj)">编辑</el-button>
+          <el-button type="text" @click="propertyDel(obj)">删除</el-button>
+          <el-button type="text" @click="propertyLocation(obj)">定位</el-button>
+        </template>
+      </miTable>
+    </div>
+  </div>
+</template>
+<script>
+import { LeaseManageDic } from "@/utils/dic/leaseManage";
+import miForm from "@/components/Form";
+import miTable from "@/components/Table";
+import leaseManageApi from "@/service/api/leaseManageApi";
+import commonFun from "@/utils/commonFun.js";
+
+export default {
+  components: { miForm, miTable },
+  data() {
+    return {
+      model: {},
+      leaseManageForm: {
+        ref: "leaseManageForm",
+        size: "small",
+        menuPosition: "right",
+        menuBtn: false,
+        forms: [
+          {
+            type: "input",
+            label: "车牌号码",
+            prop: "houseNumber",
+            placeholder: "请输入",
+            clearable: true,
+            span: 4
+          },
+          {
+            type: "datetime",
+            label: "入场时间",
+            prop: "houseName",
+            clearable: true,
+            span: 4
+          },
+          {
+            type: "datetime",
+            label: "出场时间",
+            prop: "housePrice",
+            clearable: true,
+            span: 4
+          },
+          {
+            type: "input",
+            label: "停车场名称",
+            prop: "houseStatus",
+            placeholder: "请输入",
+            clearable: true,
+            span: 4,
+          },
+          {
+            prop: "btn",
+            span: 4,
+            formslot: true,
+            width: 55
+          }
+        ]
+      },
+      leaseManageTable: {
+        ref: "leaseManageTable",
+        serverMode: {
+          url: leaseManageApi.getHouseList,
+          data: {
+            pageNum: 1,
+            pageSize: 10
+          },
+          props: {
+            listKey: "list",
+            total: "total",
+            pageSize: "pageSize",
+            pageNum: "pageNum"
+          }
+        },
+        operation: {
+          width: 200
+        },
+        columnConfig: [
+          {
+            prop: "houseNumber",
+            label: "房产编号",
+            width: 200
+          },
+          {
+            prop: "houseName",
+            label: "房产名称"
+          },
+          {
+            prop: "spaceName",
+            label: "空间位置"
+          },
+          {
+            prop: "houseStatus",
+            label: "房产状态",
+            formatter: function(row, column) {
+              let HouseStatus = LeaseManageDic.HouseStatus;
+              let rowValue = row[column.property];
+              let res = _.find(HouseStatus, (cur, key, obj) => {
+                return cur.value === rowValue;
+              });
+              return res ? res.label : "";
+            }
+          },
+          {
+            prop: "houseArea",
+            label: "面积 m²"
+          },
+          {
+            prop: "housePrice",
+            label: "总价",
+            formatter(row, column) {
+              let pirceTypeLabel = "";
+              if (typeof row.priceType == "number") {
+                pirceTypeLabel =
+                  LeaseManageDic.PriceType[row.priceType - 1].label;
+              }
+              return `${row[column.property]} ${pirceTypeLabel}`;
+            }
+          },
+          {
+            prop: "qysj",
+            label: "签约时间"
+          },
+          {
+            prop: "zhxm",
+            label: "租户姓名"
+          }
+        ],
+        uiConfig: {
+          height: "auto",
+          customTopPosition: "right",
+          selection: true
+        }
+      }
+    };
+  },
+  methods: {
+    submit() {},
+    resetChange() {},
+    deleteRow(ids) {
+      leaseManageApi.removeHouse({ houseIds: ids }).then(res => {
+        this.refreshTable();
+      });
+    },
+    addedProperty(obj) {
+      this.$router.push({
+        name: "editHouseProperty"
+      });
+    },
+    bulkImport(obj) {
+      this.$router.push({ name: "bulkimporthouseproperty" });
+    },
+    bulkDel({ selectedData }) {
+      if (!selectedData.length) {
+        commonFun.deleteTip(this, false, "请选择数据");
+        return;
+      }
+      let ids = _.reduce(
+        selectedData,
+        (result, cur, curindex) => {
+          return result + "," + cur.houseId;
+        },
+        ""
+      );
+      commonFun.deleteTip(
+        this,
+        true,
+        "确定要删除吗?",
+        () => {
+          this.deleteRow(ids);
+        },
+        () => {}
+      );
+    },
+    bulkEdit(obj) {
+      console.log(obj);
+    },
+    propertyDetail({ scopeRow: { $index, row, _self } }) {
+      leaseManageApi.houseDetails(row).then(res => {
+        this.$router.push({
+          name: "editHouseProperty",
+          params: {
+            extraOptions: {
+              disabled: true
+            },
+            model: _.cloneDeep(res)
+          }
+        });
+      });
+    },
+    propertyEdit({ scopeRow: { $index, row, _self } }) {
+      this.$router.push({
+        name: "editHouseProperty",
+        params: {
+          model: _.cloneDeep(row)
+        }
+      });
+    },
+    propertyDel({ scopeRow: { $index, row, _self } }) {
+      commonFun.deleteTip(
+        this,
+        true,
+        "确定要删除吗?",
+        () => {
+          this.deleteRow(row.houseId);
+        },
+        () => {}
+      );
+    },
+    propertyLocation(obj) {
+      console.log(obj);
+    },
+    search(...args) {
+      this.$refs[this.leaseManageForm.ref].getFormModel(res => {
+        console.log("model", res);
+      });
+      console.log("搜索", ...args);
+    },
+    clearForm(...args) {
+      console.log("清空", ...args);
+      this.$refs[this.leaseManageForm.ref].resetForm();
+    },
+    refreshTable() {
+      this.$refs[this.leaseManageTable.ref].refreshTable();
+    }
+  }
+};
+</script>
+<style lang='less'>
+#houseproperty-form .el-form-item {
+  margin-bottom: 0;
+}
+</style>
---
+++ src/router/parkinglot-manage.js
@@ -0,0 +1,28 @@
+export default [{
+    path: '/parkinglotmanage',
+    name: 'parkinglotmanage',
+    meta: { title: '停车场管理' },
+    component: () => import('@/pages/parkingLotManage'),
+    redirect: '/parkinglotmanage/accessrecord',
+    children: [{
+        path: 'accessrecord',
+        name: 'accessrecord',
+        meta: { title: '车辆出入记录' },
+        component: () => import('@/pages/parkingLotManage/accessRecord')
+    }, {
+        path: 'vehiclerecord',
+        name: 'vehiclerecord',
+        meta: { title: '过车记录' },
+        component: () => import('@/pages/parkingLotManage/vehiclereRecord'),
+    }, {
+        path: 'parkinglotrecord',
+        name: 'parkinglotrecord',
+        meta: { title: '场区记录' },
+        component: () => import('@/pages/parkingLotManage/parkingLotRecord'),
+    }, {
+        path: 'feebasedinfo',
+        name: 'feebasedinfo',
+        meta: { title: '收费信息' },
+        component: () => import('@/pages/parkingLotManage/feeBasedInfo'),
+    }],
+}]
