38a89c817598a5989b12920666385e6538fc4f68
lxh <283700113@qq.com>
2019-12-24 11:51:11 +0800

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -11,16 +11,16 @@ import vibeWeb from './vibeWeb'                               // 旧项目
 import DigitalParkRouter from './digital-park-router'         // 数字园区
 import EnergyRouter from './energy-router'                    // 能源管理
 import LeaseManage from './lease-manage'                      // 租赁管理
-import CleaningManage from './cleaning-manage'                //保洁管理
-import CostManage from './cost-manage'                        //费用管理
-import ReceiveSupportManage from './receive-support-manage'   //收支管理
-import PurchaseManage from './purchase-manage'                //采购管理
-import StockManage from './stock-manage'                      //库存管理
-import taskManage from './task-manage'                        //任务管理
+import CleaningManage from './cleaning-manage'                // 保洁管理
+import CostManage from './cost-manage'                        // 费用管理
+import ReceiveSupportManage from './receive-support-manage'   // 收支管理
+import PurchaseManage from './purchase-manage'                // 采购管理
+import StockManage from './stock-manage'                      // 库存管理
+import taskManage from './task-manage'                        // 任务管理
 
 import visitorManage from './visitor-manage'                  // 访客管理
 import ParkingLotManage from './parkinglot-manage'            // 停车场管理
-import contractManage from './contract-manage'                //合同管理
+import contractManage from './contract-manage'                // 合同管理
 
 import helloRouter from './hello-router'
 
@@ -41,7 +41,6 @@ Vue.use(Router)
 const router = new Router({
   "routes": DigitalRouters
 })
-//
 
 router.beforeEach((to, from, next) => {
   if (typeof to.meta.loginCheck === 'boolean' && !to.meta.loginCheck) {
