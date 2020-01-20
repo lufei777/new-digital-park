622f090c68d4acb6a224694ac86977020adf2126
guanys <616393451@qq.com>
2020-01-20 16:24:50 +0800

    add client-dashborad coms

---
+++ src/pages/digitalPark/coms/security/inspection.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="access-control">
+    <div ></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'accessControl',
+    components: {
+    },
+    data () {
+      return {
+      }
+    },
+    computed:{
+    },
+    watch:{
+    },
+    methods: {
+    },
+    mounted(){
+    }
+  }
+</script>
+
+<style lang="less">
+</style>
