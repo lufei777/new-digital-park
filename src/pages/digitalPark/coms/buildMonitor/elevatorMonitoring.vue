94ccfd8ca90bffb9119dc0ca26c771451308f986
guanys <616393451@qq.com>
2020-01-21 17:38:44 +0800

    update

--- src/pages/digitalPark/coms/buildMonitor/elevatorMonitoring.vue
+++ src/pages/digitalPark/coms/buildMonitor/elevatorMonitoring.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="elevator-monitor-coms">
-    <div class="chart-box">
+    <div class="chart-box flex">
       <!--<div class="chart1" ref="myChart1"></div>-->
-      <vcharts :chartConfig="chartConfig" :chartData='chartConfig.chartData' class="chart1"></vcharts>
-      <vcharts :chartConfig="chartConfig" :chartData='chartConfig.chartData' class="chart1"></vcharts>
+      <div class="chart-box-item">
+        <vcharts :chartConfig="chartConfig" :chartData='chartConfig.chartData' class="chart1"></vcharts>
+        <span>直梯</span>
+      </div>
+      <div class="chart-box-item">
+        <vcharts :chartConfig="chartConfig2" :chartData='chartConfig.chartData' class="chart1"></vcharts>
+        <span>扶梯</span>
+      </div>
+
     </div>
     <div>{{moduleItem.moduleName}}</div>
   </div>
@@ -20,27 +27,54 @@
     },
     props: ["moduleItem"],
     data() {
+      let label={
+        normal: {
+          show: false,
+            position: 'center'
+        },
+        emphasis: {
+          show: true,
+            textStyle: {
+            fontSize: '30',
+              fontWeight: 'bold'
+          }
+        }
+      }
       return {
         chartConfig: {
           type: "ring",
-          chartSettings:{
+          legend:{
+            textStyle:{
+              color:'#8FD3FA'
+            }
+          },
+          settings:{
             offsetY: 130,
-            radius: [55, 80],
+            radius: [50, 70],
+            label:label,
           },
           chartData: {
             columns: [
               "label",
               "value",
             ],
-            rows: [
-              {
-              label:'上升',value:1
-            },{
-              label:'下降',value:2
-            },{
-              label:'停止',value:3
-            }
-            ]
+            rows: [{}]
+          }
+        },
+        chartConfig2: {
+          type: "ring",
+          legendVisible:false,
+          settings:{
+            offsetY: 130,
+            radius: [50, 70],
+            label:label,
+          },
+          chartData: {
+            columns: [
+              "label",
+              "value",
+            ],
+            rows: [{}]
           }
         }
       };
@@ -53,6 +87,20 @@
         this.initChart(res)
       },
       initChart(res){
+        this.chartConfig.chartData.rows=[{
+            label:'上升',value:res[0].up
+          },{
+            label:'下降',value:res[0].down
+          },{
+            label:'停止',value:res[0].stop
+        }]
+        this.chartConfig2.chartData.rows=[{
+          label:'上升',value:res[1].up
+        },{
+          label:'下降',value:res[1].down
+        },{
+          label:'停止',value:res[1].stop
+        }]
         // let myChart = echarts.init(this.$refs.myChart1);
         // let legendData = [`开启直梯：${res[0].data[0].value}`,`开启直梯：${res[0].data[0].value}`];
         // let dataList = [];
@@ -95,8 +143,12 @@
       width:100%;
       height:100%;
     }
-    .chart1{
+    .chart-box-item{
       width:50%;
+      height:90%;
+    }
+    .chart1{
+      width:90%;
       height:80%;
     }
   }
