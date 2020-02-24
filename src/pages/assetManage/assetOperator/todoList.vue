69135f41ae4cf4f17451064318fca618ab47ef53
guanys <616393451@qq.com>
2020-02-24 15:30:35 +0800

    assetuse api

--- src/pages/assetManage/assetOperator/todoList.vue
+++ src/pages/assetManage/assetOperator/todoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="todo-list panel-container panel">
-    <el-tabs type="border-card" @tab-click="onClickTab" >
+    <el-tabs type="border-card" @tab-click="onClickTab" v-model="activeTab">
       <el-tab-pane label="待办">
         <TodoComs fromFlag="1"/>
       </el-tab-pane>
@@ -12,6 +12,7 @@
 </template>
 
 <script>
+  import {mapState} from 'vuex'
   import TodoComs from '../coms/todo'
   export default {
     name: 'TodoList',
@@ -20,7 +21,18 @@
     },
     data () {
       return {
-        // activeTab:1
+        activeTab:'0'
+      }
+    },
+    computed:{
+      ...mapState({
+        todoTab:state=>state.digitalPark.todoTab
+      })
+    },
+    watch:{
+      todoTab(){
+        debugger
+        this.activeTab = this.todoTab
       }
     },
     methods: {
