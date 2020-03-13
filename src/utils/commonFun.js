48ad37a73d97447442092a955b8d24b17d667129
liuxiaohang <283700113@qq.com>
2020-03-13 11:03:42 +0800

    修改租赁管理删除报错 axios返回处理

--- src/utils/commonFun.js
+++ src/utils/commonFun.js
@@ -73,38 +73,6 @@ class commonFun {
     airConditioner,
     electricTransformerAndDistribution
   }
-  //this,删除的id,没有id时的提示信息，点击确定的回调函数
-  deleteTip(that, deleteId, msgTip, sureCallBack, cancelCallBack) {
-    if (!deleteId) {
-      that.$message({
-        type: 'warning',
-        message: msgTip,
-        duration: 1000
-      });
-      return;
-    }
-    that.$confirm('确定要删除吗?', '提示', {
-      confirmButtonText: '确定',
-      cancelButtonText: '取消',
-      type: 'warning'
-    }).then(() => {
-      sureCallBack()
-    }).catch((err) => {
-      if (err === 'cancel') {
-        that.$message({
-          type: 'info',
-          message: '已取消删除',
-        });
-        cancelCallBack && cancelCallBack()
-      } else {
-        that.$message({
-          type: 'error',
-          message: '出错,请在控制台查看错误信息',
-        });
-        console.error(err);
-      }
-    });
-  }
 
   menuData = {
     "id": 1,
@@ -1653,6 +1621,39 @@ class commonFun {
     type2: '图片'
   }]
 
+  //this,删除的id,没有id时的提示信息，点击确定的回调函数
+  deleteTip(that, deleteId, msgTip, sureCallBack, cancelCallBack) {
+    if (!deleteId) {
+      that.$message({
+        type: 'warning',
+        message: msgTip,
+        duration: 1000
+      });
+      return;
+    }
+    that.$confirm('确定要删除吗?', '提示', {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      type: 'warning'
+    }).then(() => {
+      sureCallBack && sureCallBack();
+    }).catch((err) => {
+      if (err === 'cancel') {
+        that.$message({
+          type: 'info',
+          message: '已取消删除',
+        });
+        cancelCallBack && cancelCallBack();
+      } else {
+        that.$message({
+          type: 'error',
+          message: '出错,请在控制台查看错误信息',
+        });
+        console.error(err);
+      }
+    });
+  }
+
   // 跳转链接
   loadOldPage(item, routeOldProject) {
     // 直接通过浏览器改变href跳转
@@ -1719,21 +1720,21 @@ class commonFun {
   }
 
   //设置菜单index
-  setMenuIndex(item){
-    let arr = ['defaultPage','digitalPark/dashboardHomePage','stockInApply']
-    let flag=false
-    if(item.routeAddress){
-      arr.map((str)=>{
-        if(item.routeAddress.indexOf(str)!=-1){
-          flag=true
+  setMenuIndex(item) {
+    let arr = ['defaultPage', 'digitalPark/dashboardHomePage', 'stockInApply']
+    let flag = false
+    if (item.routeAddress) {
+      arr.map((str) => {
+        if (item.routeAddress.indexOf(str) != -1) {
+          flag = true
         }
       })
-      if(flag){
-        return item.id+item.routeAddress
-      }else{
+      if (flag) {
+        return item.id + item.routeAddress
+      } else {
         return item.routeAddress
       }
-    }else{
+    } else {
       return item.id
     }
   }
@@ -1750,15 +1751,15 @@ class commonFun {
     }
   }
 
-  setShortcutList(shortcut){
-    localStorage.setItem("shortcutList",JSON.stringify(shortcut))
+  setShortcutList(shortcut) {
+    localStorage.setItem("shortcutList", JSON.stringify(shortcut))
   }
 
-  goToZGManage(item){
+  goToZGManage(item) {
     //中钢物业管理客户端来处理
-    if(item.name=="物业系统"){
+    if (item.name == "物业系统") {
       window.goToZGManage()
-      return ;
+      return;
     }
   }
 }
