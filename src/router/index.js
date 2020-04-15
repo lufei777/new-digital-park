89e5fefe5812e0e1fa2c6094732f7d68322ccb3c
刘晓航 <283700113@qq.com>
2020-04-15 13:53:35 +0800

    上传修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import axois from '../service/axios/AxiosInterceptors'
 import { flattenDeep } from 'utils/czUtils'
 import store from '../vuex/store'
 
+import helloRouter from './hello-router'
 // 公共路由
 import vibeWeb from './vibeWeb'                               // 旧项目
 
 // 项目私有路由
-import AssetManage from './asset-manage'          // 资产管理
+import AssetManage from './asset-manage'                      // 资产管理
 import DigitalParkRouter from './digital-park-router'         // 数字园区
 import EnergyRouter from './energy-router'                    // 能源管理
 import LeaseManage from './lease-manage'                      // 租赁管理
@@ -25,28 +25,17 @@ import contractManage from './contract-manage'                // 合同管理
 import NewsManage from './news-manage'                        // 消息管理
 import MessageManage from './message-manage'                  // 信息发布
 import personalManage from './personal-manage';               // 人事管理
-import thirdParty from './third-party';
+import thirdParty from './third-party';                       // 物业管理
 import RevenueExpendManage from './revenue-expend-manage';    // 收支管理
+import ExpoetData from './commonRouter/exportData';           // 导出数据
 
-import ExpoetData from './commonRouter/exportData';    // 导出数据
-
-import helloRouter from './hello-router'
 
 // 数字园区
-let DigitalRouters = flattenDeep(
-  [
-    DigitalParkRouter, AssetManage,
-    EnergyRouter, vibeWeb,
-    LeaseManage, CleaningManage,
-    CostManage,
-    PurchaseManage, StockManage,
-    visitorManage, ParkingLotManage,
-    taskManage, contractManage, NewsManage,
-    MessageManage, personalManage,
-    thirdParty, RevenueExpendManage,
-    ExpoetData
-  ]
-);
+let DigitalRouters = flattenDeep([
+  DigitalParkRouter, AssetManage, EnergyRouter, vibeWeb, LeaseManage, CleaningManage, CostManage,
+  PurchaseManage, StockManage, visitorManage, ParkingLotManage, taskManage, contractManage, NewsManage,
+  MessageManage, personalManage, thirdParty, RevenueExpendManage, ExpoetData
+]);
 
 Vue.use(Router)
 const router = new Router({
@@ -67,8 +56,8 @@ router.beforeEach((to, from, next) => {
   // if (to.path != '/vibe-web') {
   //   store.commit('digitalPark/activeMenuIndex', to.path);
   // }
-  if(window.name === 'largeScreen'){
-    store.commit("digitalPark/menuList",JSON.parse(localStorage.menuList));
+  if (window.name === 'largeScreen') {
+    store.commit("digitalPark/menuList", JSON.parse(localStorage.menuList));
   }
 })
 export default router
