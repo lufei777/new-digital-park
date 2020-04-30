7a782cb3cfd524553cbfa81ac18c7f075ca1afeb
2458648898@qq.com <2458648898@qq.com>
2020-04-30 10:04:47 +0800

    能源概览 安防巡检排版

---
+++ src/pages/commonProject/statisAnalysis/device - 副本.vue
@@ -0,0 +1,206 @@
+<template>
+  <div class="device-analysis panel-container">
+    <div class="device-chart-box">
+      <div class="chart-box radius-shadow" style="margin-right:1%">
+        <div ref="myChart" class="my-chart"></div>
+      </div>
+      <el-dialog :visible.sync="showDialog" width="30%">
+        <el-button type="primary" @click="onClickExportBtn">导出报表</el-button>
+        <div class="table-box">
+          <el-table :data="tableData" border>
+            <el-table-column prop="monitorId" label="编号" align="right"></el-table-column>
+            <el-table-column prop="name" label="类型" align="right"></el-table-column>
+          </el-table>
+        </div>
+      </el-dialog>
+      <div class="chart-box radius-shadow">
+        <div class="my-chart" ref="myColumnEcharts"></div>
+      </div>
+    </div>
+    <div class="device-table-box panel">
+      <el-table :data="deviceTableData" border>
+        <el-table-column prop="index" label="序号"></el-table-column>
+        <el-table-column prop="name" label="状态"></el-table-column>
+        <el-table-column prop="value" label="个数"></el-table-column>
+        <el-table-column prop="ratio" label="占比"></el-table-column>
+      </el-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import CommonApi from "api/common";
+import ChartUtils from "utils/chartUtils";
+import commonFun from 'utils/commonFun';
+export default {
+  name: "DeviceAnalysis",
+  components: {},
+  data() {
+    return {
+      deviceData: {},
+      myChart: "",
+      tableData: [],
+      showDialog: false,
+      chartIndex: 0,
+      deviceTableData: []
+    };
+  },
+  methods: {
+    async getMonitorState() {
+      let res = await CommonApi.getMonitorState();
+      this.createColumnEcharts(res);
+      this.createPieEcharts(res);
+
+      let tmp = [];
+      let sum = 0;
+      res.values.map((item, index) => {
+        sum += item.value;
+        tmp.push({
+          index: index + 1,
+          name: item.name,
+          value: item.value
+        });
+      });
+      this.deviceTableData = tmp;
+      this.deviceTableData.map((item, index) => {
+        item.ratio = Number((item.value / sum) * 100).toFixed(2) + "%";
+      });
+    },
+    createPieEcharts(res) {
+      this.myChart = this.$echarts.init(this.$refs.myChart);
+      let titleText = "传感器状态统计";
+      let legendData = res.legend;
+      let seriesData = res.values;
+      let seriesName = "监控器状态";
+      let option = {
+        title: {
+          x: "center"
+        },
+        legend: {
+          left: "left",
+          top: 50
+        },
+        color: ["dimGrey", "green", "orange", "red"],
+        toolbox: {
+          show: true,
+          feature: Object.assign({}, commonFun.chartsToolBox('assetStateTip').feature),
+          tooltip: commonFun.chartsToolBox('assetStateTip').tooltip
+        }
+      };
+      console.log("createPieEcharts -> option", option)
+      let data = {
+        titleText,
+        legendData,
+        seriesData,
+        seriesName
+      };
+      ChartUtils.hollowPieChart(this.myChart, data);
+      this.myChart.setOption(option);
+      let that = this;
+      this.myChart.on("click", function (val) {
+        that.chartIndex = val.dataIndex;
+        that.getTableData(val.dataIndex);
+      });
+    },
+    createColumnEcharts(data) {
+      let myColumnEcharts = this.$echarts.init(this.$refs.myColumnEcharts);
+      let option = {
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "shadow"
+          }
+        },
+        xAxis: [
+          {
+            type: "category",
+            data: data.legend
+          }
+        ],
+        yAxis: [
+          {
+            type: "value",
+            axisLabel: {
+              formatter: "{value} 个"
+            }
+          }
+        ],
+        series: [
+          {
+            name: "传感器状态",
+            type: "bar",
+            data: data.values,
+            itemStyle: {
+              normal: {
+                color: function (params) {
+                  var colorList = ["dimGrey", "green", "orange", "red"];
+                  return colorList[params.dataIndex];
+                }
+              }
+            }
+          }
+        ]
+      };
+      window.onresize = myColumnEcharts.resize;
+      myColumnEcharts.setOption(option);
+    },
+    async getTableData(index) {
+      let res = await CommonApi.getDeviceTableData({
+        filter: "state",
+        index
+      });
+      let tmp = [];
+      res.monitorIds.map((item, index) => {
+        tmp.push({
+          monitorId: item,
+          name: res.captions[index]
+        });
+      });
+      this.tableData = tmp;
+      this.showDialog = true;
+    },
+    onClickExportBtn() {
+      let url = `/vibe-web/statistics/getMonitorDetailsExport?filter=state&index=${this.chartIndex}`;
+      location.href = url;
+    }
+  },
+  mounted() {
+    this.getMonitorState();
+  }
+};
+</script>
+
+<style lang="less">
+.device-analysis {
+  // background: @white;
+  .chart-box {
+    background: @white;
+    width: 49.5%;
+    height: 450px;
+    // margin:0 auto;
+    // overflow: hidden;
+    float: left;
+  }
+  .my-chart {
+    height: 450px;
+  }
+  .device-table-box {
+    padding: 20px;
+    margin-top: 20px;
+  }
+
+  .el-dialog__body {
+    height: 500px;
+    overflow: hidden;
+  }
+  .el-button {
+    float: right;
+    margin: 10px;
+  }
+  .table-box {
+    overflow: auto;
+    height: 450px;
+    clear: both;
+  }
+}
+</style>
---
+++ src/pages/commonProject/statisAnalysis/historyStatis - 副本.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="history-statis">
+    <MonitorChoose :showDateType="true" :showTwoMonitor="false"/>
+    <MonitorModal />
+    <el-button type="primary" class='handle-btn' @click="onClickBtn">处理</el-button>
+    <div ref="myChart" class="my-chart"></div>
+    <el-table :data="tableData" border height="350px">
+      <el-table-column prop="time" label="时间" align="right" width="100px"></el-table-column>
+      <el-table-column prop="max1" label="最大值(KWH)" align="right"></el-table-column>
+      <el-table-column prop="min1" label="最小值(KWH)" align="right"></el-table-column>
+      <el-table-column prop="avg1" label="平均值(KWH)" align="right"></el-table-column>
+      <el-table-column prop="std1" label="均方差(KWH)" align="right"></el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+  import {mapState} from 'vuex'
+  import echarts from 'echarts'
+  import MonitorChoose from '../coms/monitorChoose'
+  import MonitorModal from '../../../components/monitorModal/index'
+  import CommonApi from '../../../service/api/common'
+  import ChartUtils from '../../../utils/chartUtils'
+  export default {
+    name: 'DeviceAnalysis',
+    components: {
+      MonitorChoose,
+      MonitorModal
+    },
+    data () {
+      return {
+        tableData:[],
+        tableText:['B-ALE-1-a 照明 (KWH)'],
+        myChart:''
+      }
+    },
+    computed:{
+      ...mapState({
+        startTime:state=>state.analysis.startTime,
+        endTime:state=>state.analysis.endTime,
+        monitor:state=>state.analysis.monitor1,
+        filterType:state=>state.analysis.filterType
+      }),
+    },
+    watch:{
+      monitor(){
+        this.tableText[0]=this.monitor.text
+      },
+    },
+    methods: {
+      async getHistoryStatisData(){
+        let params={
+          startTime:this.startTime,
+          lastTime:this.endTime,
+          monitorId:this.monitor.id,
+          filterType:this.filterType
+        }
+        let res =  await CommonApi.getHistoryStatisData(params)
+         this.initChart(res)
+         this.initTableData(res)
+      },
+      onClickBtn(){
+        this.getHistoryStatisData()
+      },
+      initChart(res){
+        let series= [{
+            data:res.maxValues,
+            type:'bar',
+            name:'最大值'
+            },{
+            data:res.minValues,
+            type:'bar',
+            name:'最小值'
+          },{
+            data:res.avgValues,
+            type:'bar',
+            name:'平均值'
+          },{
+            data:res.stdValues,
+            type:'bar',
+            name:'均方差'
+        }]
+        let legendData=['最大值','最小值','平均值','均方差']
+          this.myChart=this.$echarts.init(this.$refs.myChart)
+          let xAxis =res.xAxis
+          let data={
+            titleText:'',
+            legendData,
+            xAxis,
+            series,
+            yAxis:res.unit1
+          }
+          ChartUtils.handleBarChart(this.myChart,data)
+      },
+      initTableData(res){
+        let tmp=[]
+        res.xAxis.map((item,index)=>{
+          tmp.push({
+            time:item,
+            max1:res.maxValues[index],
+            min1:res.minValues[index],
+            avg1:res.avgValues[index],
+            std1:res.stdValues[index],
+          })
+        })
+        this.tableData=tmp
+      }
+    },
+    created(){
+      this.$store.dispatch("analysis/setDefaultNode");
+    },
+    beforeDestroy(){
+      this.$store.dispatch("analysis/resetStates"); //重置vuex
+    },
+    mounted(){
+       setTimeout(()=>this.getHistoryStatisData(),500)
+    }
+  }
+</script>
+
+<style lang="less">
+  .history-statis{
+    margin-top: 85px;
+    padding:0 20px;
+    .chart-box{
+      width:80%;
+      margin:20px auto;
+    }
+    .my-chart{
+      width: 60%;
+      height:350px;
+      clear: both;
+      float: left;
+    }
+    .choose-box{
+      padding:20px 0;
+      border-bottom: 1px solid #eaeaea;
+    }
+    .block{
+      margin:0 40px;
+    }
+    .choose-tip{
+      margin-left: 100px;
+      width:80px;
+      text-align: right;
+    }
+    .handle-btn{
+      margin:10px;
+      float: right;
+    }
+    .el-table{
+      /*clear: both;*/
+      float: right;
+      width:40%;
+    }
+    .el-table th.gutter{
+      width:16px;
+    }
+  }
+</style>
