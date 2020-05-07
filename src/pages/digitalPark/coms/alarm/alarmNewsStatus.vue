b48176562a31f98936509ac1a0cd1de40b7ce03b
2458648898@qq.com <2458648898@qq.com>
2020-05-07 18:59:02 +0800

    伊甸城预警报警

---
+++ src/pages/digitalPark/coms/alarm/alarmNewsStatus.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="alarm-news-status">
+    <div class="my-chart">
+      <div class="alarm-news-header flex">
+        <span style="width:10%">序号</span>
+        <span style="width:40%">事件源</span>
+        <span>时间</span>
+        <span>状态</span>
+      </div>
+      <ul v-if="alarmNewsData.length!=0">
+        <li v-for="(item,index) in alarmNewsData" :key="index" class="ganged-log-li flex">
+          <span style="width:10%">{{index+1}}</span>
+          <span style="width:40%">{{item.alertType}}</span>
+          <span>{{item.timeText}}</span>
+          <span>{{item.alertTypeText}}</span>
+        </li>
+      </ul>
+      <div class="no-data" v-else>暂无数据</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CommonApi from "@/service/api/common";
+export default {
+  name: "alarmNewsStatus",
+  props: ["moduleItem"],
+  data() {
+    return {
+      alarmNewsData: []
+    };
+  },
+  methods: {
+    async getAlarmMessageList() {
+      let res = await CommonApi.getAlarmMessageList({
+        pageNum: 1,
+        pageCount: 6,
+        start: "",
+        end: ""
+      });
+      if (res.rows.length != 0) {
+        this.alarmNewsData = res.rows.slice(0,5);
+        this.alarmNewsData.map((item, index) => {
+          let alertStr = item.caption.split("->");
+          let alertDevice = alertStr.pop();
+          let alertType = alertStr.pop();
+          item.alertType = alertType;
+          item.timeText = item.startTime.slice(0,10)
+          item.alertTypeText =
+            item.handled == 1
+              ? "未处理"
+              : item.handled == 2
+              ? "已处理"
+              : "未处理";
+        });
+      }
+    }
+  },
+  mounted() {
+    this.getAlarmMessageList();
+  }
+};
+</script>
+
+<style lang="less">
+.alarm-news-status {
+  .my-chart {
+    margin-top: 10px;
+    box-sizing: border-box;
+    .alarm-news-header {
+      width: 100%;
+      height: 50px;
+      font-size: 14px;
+      background: rgba(17, 29, 33, 0.5);
+      text-align: center;
+      span {
+        width: 25%;
+        float: left;
+        height: 50px;
+        line-height: 50px;
+      }
+    }
+    .no-data {
+        text-align: center;
+        height: 50px;
+        line-height: 50px;
+    }
+  }
+  .ganged-log-li {
+    width: 100%;
+    height: 40px;
+    font-size: 14px;
+    color: @white;
+    text-align: center;
+    span {
+      width: 25%;
+      display: block;
+      float: left;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      height: 30px;
+      line-height: 30px;
+    }
+    span:last-child {
+      color: #01eafe;
+    }
+  }
+}
+</style>
