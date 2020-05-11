88c50c2f59d59e4fcc0fba98075c486c8ed209ad
guanys <616393451@qq.com>
2020-05-11 11:14:28 +0800

    zg

---
+++ src/pages/messageManage/publish/index.vue
@@ -0,0 +1,201 @@
+<template>
+  <div class="message-publish-manage panel-container">
+    <div class="condition-box radius-shadow ">
+      <!--<z-form-->
+        <!--:ref="formData.ref"-->
+        <!--:options="formData"-->
+        <!--v-model="model"-->
+        <!--@submit="submit"-->
+        <!--@reset-change="resetChange"-->
+      <!--&gt;-->
+        <!--<template slot="btn" slot-scope="obj">-->
+          <!--<div>-->
+            <!--<el-button :disabled="obj.disabled" type="primary" @click="onClickSearchBtn(obj)">搜索</el-button>-->
+            <!--<el-button :disabled="obj.disabled" @click="clearForm(obj)">清除</el-button>-->
+          <!--</div>-->
+        <!--</template>-->
+      <!--</z-form>-->
+      <div class="item-group">
+        <label for="">发布编号：</label>
+        <el-input type="text" />
+      </div>
+      <div class="item-group">
+        <label for="">发布名称：</label>
+        <el-input type="text" />
+      </div>
+      <div class="item-group">
+        <label for="">发布日期：</label>
+        <el-input type="text" />
+      </div>
+      <div class="item-group">
+        <el-button  type="primary" @click="onClickSearchBtn(obj)">搜索</el-button>
+        <el-button  @click="clearForm(obj)">清除</el-button>
+      </div>
+    </div>
+
+    <div class="warehouse-manage-table panel">
+      <div class="operator-btn-box flex-row-reverse">
+        <el-button  type="primary">批量删除</el-button>
+        <el-button  type="primary">导出</el-button>
+        <el-button  type="primary">导入</el-button>
+        <el-button  type="primary">新增</el-button>
+      </div>
+      <z-table :ref="tableData.ref" :options="tableData">
+      </z-table>
+    </div>
+  </div>
+</template>
+
+<script>
+  import CommonFun from "@/utils/commonFun";
+  export default {
+    name: "publishManage",
+    data() {
+      return {
+        model: {},
+        formData: {
+          ref: "formData",
+          labelWidth: "100",
+          size: "medium",
+          menuPosition: "right",
+          menuBtn: false,
+          // labelPosition: "left",
+          forms: [
+            {
+              type: "input",
+              label: "发布编号",
+              prop: "id",
+              placeholder: "请输入",
+              clearable: true,
+              span: 4,
+              minRows: 0
+            },
+            {
+              type: "input",
+              label: "发布名称",
+              prop: "name",
+              placeholder: "请输入",
+              clearable: true,
+              span: 4
+            },
+            // {
+            //   type: "input",
+            //   label: "信息状态",
+            //   prop: "type",
+            //   placeholder: "请输入",
+            //   clearable: true,
+            //   span: 4
+            // },
+            // {
+            //   prop: "",
+            //   formslot: true,
+            //   span: 12
+            // },
+            {
+              type: "input",
+              label: "发布日期",
+              prop: "status",
+              // clearable: true,
+              span: 4,
+              format: "yyyy-MM-dd",
+              valueFormat: "timestamp"
+            },
+            // {
+            //   prop: "",
+            //   formslot: true,
+            //   span: 6
+            // },
+            // {
+            //   type: "input",
+            //   label: "添加日期",
+            //   prop: "date",
+            //   placeholder: "请输入",
+            //   span: 4
+            // },
+            {
+              prop: "btn",
+              span: 6,
+              pull: 2,
+              formslot: true
+              // width: "34px"
+            }
+          ]
+        },
+        tableData: {
+          ref: "tableData",
+          customTop: true,
+          data: [],
+          columnConfig: [],
+          uiConfig: {
+            height: "auto", //"", //高度
+            selection: true, //是否多选
+            showIndex: true,
+            pagination: {
+              //是否分页，分页是否自定义
+              layout: "total,->, prev, pager, next, jumper",
+              pageSizes: [10, 20, 50],
+              handler(pageSize, currentPage, table) {
+                _this.handleCurrentChange(currentPage);
+              }
+            }
+          },
+          btnConfig: {
+            prop: "operation",
+            label: "操作",
+            fixed: "right",
+            width: 200,
+            btns: [
+              {
+                label: "详情",
+                handler: function(row) {}
+              },
+              {
+                label: "编辑",
+                handler: function(row) {}
+              },
+              {
+                label: "删除",
+                handler: function(row) {}
+              }
+            ]
+          }
+        }
+      };
+    },
+    methods: {
+      submit() {},
+      resetChange() {},
+      getCleaningList() {
+        let res = CommonFun.messageDevice;
+        let labelList = [
+          { label: "区域编号", prop: "id" },
+          { label: "区域名称", prop: "name2" },
+          { label: "区域类型", prop: "type" },
+          { label: "区域状态", prop: "status" },
+          { label: "添加日期", prop: "date1" },
+          { label: "使用日期", prop: "date2" },
+          { label: "备注", prop: "remark" }
+        ];
+        this.tableData.columnConfig = labelList;
+        this.tableData.data = res;
+      },
+      batchDels() {},
+      addTenant() {}
+    },
+    mounted() {
+      this.getCleaningList();
+    }
+  };
+</script>
+
+<style lang="less">
+  .message-publish-manage {
+    .condition-box{
+      padding:20px;
+      margin-bottom: 20px;
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+    }
+  }
+</style>
--- static/webconfig/webconfig.js
+++ static/webconfig/webconfig.js
@@ -5,13 +5,19 @@
  * ydCity 伊甸城
  */
 
-window.__CZ_SYSTEM = '';
+window.__CZ_SYSTEM = 'zg';
 
 
 
 /**
  * __CZ_LargeScreen
- * ''     通用大屏
+ * ''  不是大屏状态
+ * 'normal'     通用大屏
  * ydCity  伊甸城
  */
-window.__CZ_LargeScreen = 'ydCity'
+window.__CZ_LargeScreen = ''
+
+
+//__CZ_SYSTEM 当前是哪个项目。
+//__CZ_LargeScreen 当前是否是大屏。不为空时，登录以后直接到大屏，打包非大屏项目时需要置为空
+
