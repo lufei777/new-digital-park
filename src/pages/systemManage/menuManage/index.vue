3c0dfdf81b4e3d06bbc3af56978c6ef644610f78
guanys <616393451@qq.com>
2020-07-02 13:46:59 +0800

    loadPage add-moduleId

--- src/pages/systemManage/menuManage/index.vue
+++ src/pages/systemManage/menuManage/index.vue
@@ -3,7 +3,7 @@
     <z-table :ref="tableConfig.ref" :options="tableConfig" class="panel">
       <template slot="operation" slot-scope="{scopeRow:{$index,row}}">
         <el-button type="text" @click="editRow(row)">编辑</el-button>
-        <el-button type="text" @click="deleteRow(row)">删除</el-button>
+        <!--<el-button type="text" @click="deleteRow(row)">删除</el-button>-->
       </template>
     </z-table>
   </div>
@@ -11,6 +11,7 @@
 </template>
 
 <script>
+  import SystemManageApi from '@/service/api/systemManage'
   export default {
     name: 'menuManage',
     components: {},
@@ -20,10 +21,10 @@
         tableConfig: {
           ref: "tableRef",
           data:[],
-          // serverMode: {
-            // url: config.serverUrl,
+          serverMode: {
+            // url: SystemManageApi.getMenuList,
             // data: {...pageInfo}
-          // },
+          },
           propsHttp: {
             list: "list",
             total: "total",
@@ -32,27 +33,45 @@
           },
           props:{
             rowKey:"id",
-            treeProps:{
-              children: 'childNode',
-              hasChildren: 'hasChildren'
-            },
           },
           treeProps:{
             children: 'childNode',
             hasChildren: 'hasChildren'
           },
           columnConfig:[{
+            label:'菜单名称',
+            prop:'name'
+          },{
             label:'菜单编号',
             prop:'code'
           },{
-            label:'菜单名称',
-            prop:'name'
+            label:'排序',
+            prop:'sequence'
+          },{
+            label:'图标',
+            prop:'icon',
+            formatter:function (row) {
+              // return row.icon
+              // console.log("<i class ='iconfont "+row.icon+"'></i>")
+              return row.icon?"<i class ='iconfont "+row.icon+"'></i>":'--'
+            }
+          },{
+            label:'可见',
+            prop:'isHide',
+            formatter:function (row) {
+              // return row.icon
+              return row.isHide==1?'可见':'隐藏'
+            }
+          },{
+            label:'菜单路径',
+            prop:'routeAddress'
           }],
           operation: {
             width: 200
           },
           uiConfig: {
             height: "auto",
+            pagination:false,
           },
           customTop: true,
           tableMethods: {},
@@ -62,21 +81,26 @@
     computed: {},
     watch: {},
     methods: {
+      async getMenuList(){
+        let res = await SystemManageApi.getMenuList()
+        this.tableConfig.data = res[0].childNode
+      },
       editRow(row){},
       deleteRow(row){},
     },
     mounted() {
-      let data = [{
-        id:1,
-        code:'001',
-        name:'运维管理',
-        childNode:[{
-          id:1001,
-          code:'1001',
-          name:'系统管理'
-        }]
-      }]
-      this.tableConfig.data = data
+      this.getMenuList()
+      // let data = [{
+      //   id:1,
+      //   code:'001',
+      //   name:'运维管理',
+      //   childNode:[{
+      //     id:1001,
+      //     code:'1001',
+      //     name:'系统管理'
+      //   }]
+      // }]
+      // this.tableConfig.data = data
     }
   }
 </script>
