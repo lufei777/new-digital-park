ad35e60224c4152ae4c7120a2132f7a544f7451b
2458648898@qq.com <2458648898@qq.com>
2020-07-17 11:31:18 +0800

    新增维保  完成bug

--- src/pages/maintenanceManage/coms/publicAddMaintenance.vue
+++ src/pages/maintenanceManage/coms/publicAddMaintenance.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="public-add-Maintenance panel">
+  <div class="public-add-Maintenance" :class="!showBoxShadow?'':'panel'">
     <div class="public-add-Maintenance-box">
       <z-form
         :ref="addMaintenanceForm.ref"
@@ -18,10 +18,14 @@
 <script>
 import { mapState } from "vuex";
 import { validateTelephoneNumber } from "@/utils/validate.js";
+import { MaintenanceManageDic, TaskManageDic } from "@/utils/dictionary.js";
 import CommonApi from "@/service/api/common";
+import TaskManageApi from "@/service/api/taskManage";
 export default {
   name: "PublicAddMaintenance",
+  props:['showBoxShadow'],
   data() {
+    let _this = this;
     return {
       model: {
         taskType: "1",
@@ -29,9 +33,9 @@ export default {
       },
       addMaintenanceForm: {
         ref: "formRef",
-        labelWidth: "120",
+        labelWidth: 100,
         labelPosition: "right",
-        menuPosition: "left",
+        menuPosition: "center",
         emptyBtn: false,
         size: "medium",
         menuBtn: true,
@@ -47,7 +51,7 @@ export default {
                 label: "工单名称",
                 prop: "taskName",
                 clearable: true,
-                span: 10,
+                span: 8,
                 rules: {
                   required: true,
                   message: "请输入工单名称",
@@ -59,7 +63,7 @@ export default {
                 label: "关联设备",
                 prop: "relatedDevice",
                 clearable: true,
-                span: 10,
+                span: 8,
                 props: {
                   label: "text",
                   value: "id",
@@ -71,7 +75,7 @@ export default {
                 label: "地点",
                 prop: "location",
                 clearable: true,
-                span: 10,
+                span: 8,
                 rules: {
                   required: true,
                   trigger: "change"
@@ -82,12 +86,13 @@ export default {
                 label: "维修类型",
                 prop: "amintenanceTypes",
                 clearable: true,
-                span: 10
+                span: 8,
+                dicData: MaintenanceManageDic.maintenanceType
               },
               // {
               //   label: "创建人",
               //   prop: "createBy",
-              //   span: 10,
+              //   span: 8,
               //   textMode: true,
               //   rules: {
               //     required: true,
@@ -99,7 +104,7 @@ export default {
                 label: "报修人",
                 prop: "repairPeople",
                 clearable: true,
-                span: 10,
+                span: 8,
                 rules: {
                   required: true,
                   //
@@ -111,7 +116,7 @@ export default {
                 label: "联系电话",
                 prop: "phoneNumber",
                 clearable: true,
-                span: 10,
+                span: 8,
                 rules: {
                   required: true,
                   validator: validateTelephoneNumber,
@@ -124,7 +129,7 @@ export default {
                 prop: "beginTime",
                 valueFormat: "yyyy-MM-dd HH:mm:ss",
                 clearable: true,
-                span: 10,
+                span: 8,
                 rules: {
                   required: true,
                   trigger: "change"
@@ -135,7 +140,7 @@ export default {
                 label: "描述",
                 prop: "description",
                 clearable: true,
-                span: 22,
+                span: 24,
                 maxlength: 255,
                 minRows: 6,
                 showWordLimit: true,
@@ -169,18 +174,110 @@ export default {
                 }
               }
             ]
+          },
+          {
+            label: "派单信息",
+            prop: "group2",
+            display: true,
+            arrow: true,
+            forms: [
+              {
+                type: "radio",
+                label: "是否收费",
+                prop: "showLink",
+                props: {
+                  label: "value",
+                  value: "id"
+                },
+                dicData: [
+                  {
+                    value: "收费",
+                    id: "1"
+                  },
+                  {
+                    value: "免费",
+                    id: "0"
+                  }
+                ],
+                valueDefault: "0",
+                span: 8,
+                rules: {
+                  required: true,
+                  trigger: "blur"
+                },
+                change: _this.onLinkRadioChange
+              },
+              {
+                type: "select",
+                label: "付款方式",
+                prop: "payWay",
+                clearable: true,
+                span: 8,
+                dicData: MaintenanceManageDic.payWayStatus,
+                valueDefault: "1",
+                rules: {
+                  required: true
+                }
+              },
+              {
+                type: "select",
+                label: "优先级",
+                prop: "urgent",
+                placeholder: "请选择",
+                clearable: true,
+                span: 8,
+                dicData: TaskManageDic.priorityType,
+                rules: {
+                  required: true
+                }
+              },
+              {
+                type: "cascader",
+                label: "部门",
+                prop: "department",
+                showAllLevels: false,
+                props: {
+                  label: "name",
+                  value: "id",
+                  children: "childNode"
+                },
+                rules: {
+                  required: true,
+                  trigger: "blur"
+                },
+                span: 8,
+                change: _this.changelistBy
+              },
+              {
+                type: "select",
+                label: "接单人",
+                prop: "designatorId",
+                placeholder: "请输入",
+                clearable: true,
+                props: {
+                  label: "name",
+                  value: "id"
+                },
+                span: 8,
+                rules: {
+                  required: true,
+                  trigger: "change"
+                }
+              },
+              {
+                type: "input",
+                label: "联系电话",
+                prop: "sendPhoneNumber",
+                clearable: true,
+                span: 8,
+                rules: {
+                  required: true,
+                  validator: validateTelephoneNumber,
+                  trigger: "change"
+                }
+              }
+            ]
           }
-          // {
-          //   label: "派单信息",
-          //   prop: "group2",
-          //   display: true,
-          //   arrow: true,
-          //   forms: [
-          //     {
-
-          //     }
-          //   ]
-          // }
         ]
       }
     };
@@ -189,15 +286,36 @@ export default {
     ...mapState("user", ["userInfo"])
   },
   methods: {
-    resetChange() {}
+    resetChange() {},
+    onLinkRadioChange() {},
+    async changelistBy({ value }) {
+      let valueData;
+      if (Array.isArray(value)) {
+        valueData = value[value.length - 1];
+      } else {
+        valueData = value;
+      }
+      let res = await TaskManageApi.listBy({
+        deptId: valueData
+      });
+      this.$refs["formRef"].setColumnByProp("designatorId", {
+        dicData: res
+      });
+    }
   },
   created() {
     CommonApi.getMonitorTree({
-      flag: 'device',
+      flag: "device",
       moduleId: localStorage.moduleId
     }).then(res => {
       this.$refs["formRef"].setColumnByProp("relatedDevice", {
         dicData: res
+      });
+    });
+
+    TaskManageApi.deptTreeList().then(res => {
+      this.$refs["formRef"].setColumnByProp("department", {
+        dicData: res[0].childNode
       });
     });
   },
@@ -208,10 +326,11 @@ export default {
 </script>
 
 <style lang="less">
+
 .public-add-Maintenance {
-  height: 100%;
+  // height: 100%;
   .public-add-Maintenance-box {
-    width: 60%;
+    width: 70%;
     margin: 0 auto;
     padding: 20px 0;
   }
