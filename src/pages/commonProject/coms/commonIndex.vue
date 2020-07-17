d79fa65a5197299257f2e2d0999c21400c382b6d
刘晓航 <283700113@qq.com>
2020-07-17 13:58:38 +0800

    路由报错commonIndex兼容处理

--- src/pages/commonProject/coms/commonIndex.vue
+++ src/pages/commonProject/coms/commonIndex.vue
@@ -35,9 +35,7 @@ export default {
     myIframe
   },
   data() {
-    let localStorageMenuData = JSON.parse(localStorage.menuList);
     return {
-      // menuData: localStorageMenuData,
       menuConfig: {
         bgColor: "#394562",
         textColor: "#B7BAC4",
@@ -64,7 +62,7 @@ export default {
     },
   },
   mounted() {
-    document.title = this.menuData.name;
+    document.title = this.menuData?.name || '';
   },
   watch: {
     menuIsCollapse(val) {
